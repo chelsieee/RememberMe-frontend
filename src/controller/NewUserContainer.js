abef4cf54cc106d9e30e21f04f145f95c1645c6a
Chelsie <chensifu4@gmail.com>
2021-04-27 20:23:07 +1000

    handle catch

--- src/controller/NewUserContainer.js
+++ src/controller/NewUserContainer.js
@@ -12,8 +12,11 @@ export const NewUserContainer = () => {
         "Access-Control-Allow-Credentials": true,
       })
       .then((res) => {
+        console.log(res)
         history.replace("/users/login");
-      });
+      }).catch((error)=>{
+        console.log(error.response.data.message)
+      })
   };
   return (
     <>
